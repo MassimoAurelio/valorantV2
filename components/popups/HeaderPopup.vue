074322142abb2d748885e22d6214837c444bc402
MassimoAurelio <massimoaureliowork@gmail.com>
2024-02-19 16:50:20 +0300

    update

--- components/popups/HeaderPopup.vue
+++ components/popups/HeaderPopup.vue
@@ -7,9 +7,11 @@ const popupStore = usePopupStore();
 <template>
   <div
     v-if="popupStore.showPopup"
-    class="fixed inset-0 flex justify-center items-center bg-black bg-opacity-50 "
+    class="fixed inset-0 flex justify-center items-center bg-black bg-opacity-50"
   >
-    <div class="flex flex-col gap-10 w-9/12 h-80 bg-slate-900 p-5 border-t border-white">
+    <div
+      class="flex flex-col gap-10 w-8/12 h-80 bg-slate-900 p-5 border-t border-white"
+    >
       <div class="flex justify-end items-start">
         <UIButton @click="popupStore.togglePopup">Cancel</UIButton>
       </div>
@@ -27,10 +29,9 @@ const popupStore = usePopupStore();
           </p>
           <button class="p-1 border border-white">
             <div
-              class="flex flex-row justify-center items-center w-48 h-12 bg-white hover:bg-slate-900"
+              class="flex flex-row justify-center items-center w-48 h-12 bg-white hover:bg-slate-900 hover:text-white border border-slate-800"
             >
-              <span></span
-              ><span class="text-black hover:text-white"> MAKE ONE </span>
+              <span></span><span> MAKE ONE </span>
             </div>
           </button>
         </div>
@@ -38,7 +39,7 @@ const popupStore = usePopupStore();
           <p class="text-white text-xs font-thin">I have a Riot account</p>
           <button class="p-1 border border-white">
             <div
-              class="flex flex-row justify-center items-center w-48 h-12 bg-red-500 hover:bg-slate-900"
+              class="flex flex-row justify-center items-center w-48 h-12 bg-red-500 hover:bg-slate-900 border border-red-400"
             >
               <span></span><span class="text-white">SIGN IN</span>
             </div>
